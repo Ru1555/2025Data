2e3fbc53b9ff570e2bf9dcfadbc66a6d3bfbc07d
Ru1555 <ruuuu155313@gmail.com>
2025-09-23 16:11:03 +0800

    0410

--- MBI.js
+++ MBI.js
@@ -1,7 +1,10 @@
 const readline = require('readline-sync');
 
-var weight = readline.question("Your weight? (kg) = ");
-var height = readline.question("Your height? (cm) = ");
+var weight = 0;
+while (weight < 10) 
+    
+
+var height = readline.questionInt("Your height? (cm) = ");
 var mbi = weight / ((height / 100) **2);
 
 console.log("Hello! YOur MBI is: " + mbi);
