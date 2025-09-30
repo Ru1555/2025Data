092bbf578efc648f962a1726fb562a688be7087c
Ru1555 <ruuuu155313@gmail.com>
2025-09-30 15:30:53 +0800

    0930

--- avg.js
+++ avg.js
@@ -1,17 +1,15 @@
 function avg(score){
-    var sum = 0, average = 0;
-    for(var i = 0; i < score.length; i++){
-        sum += score[i];
+    var sum = 0;//1
+    for(var i = 0; i < score.length; i++){//n+1
+        sum += score[i];//n
     }
 
     if(score.length > 0)
-        average = sum / score.length;
-
-    return average;
+    return average ? sum / score.length : 0;//1
     
 }
 
 //Test
 //var Score = [90, 98, 85, 100, 100, 86, 94];
-var Score = [];
+var Score = [15,56,-45,78,32,14,89];
 console.log(Score.toString()+ "Average: " + avg(Score));
