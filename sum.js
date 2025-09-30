45d62a8ac4efe6b68f729fa732bfaa3fc2118a34
Ru1555 <ruuuu155313@gmail.com>
2025-09-30 16:01:26 +0800

    0930

---
+++ sum.js
@@ -0,0 +1,27 @@
+// function sum(n)
+// {
+//     var result = 0;
+//     var i = 0;
+//     while(i++ <= n)
+//     {
+//         result += i;
+//     }
+//     return result;
+// }
+
+function sum(n)
+{
+    var result = 0;
+    var i = 0;
+    do 
+    {
+        result += i;
+    }
+    while(i++ < n);
+    return result;
+}
+
+
+//Test
+var n = 10;
+console.log("1+2+...+"+n+" = " + sum(n));
