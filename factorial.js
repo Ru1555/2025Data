e24123e2747dbb7b080f99b476b8848bfcf146f1
Ru1555 <ruuuu155313@gmail.com>
2025-10-07 14:52:49 +0800

    0252

---
+++ factorial.js
@@ -0,0 +1,23 @@
+//遞迴圈法計算階乘
+function fact(n){
+    if(n==1){
+        return 1;
+    }
+    else{
+        return n*(fact(n-1));
+    }
+}
+
+//recursive sum 1+2+3+...+n
+function sum(n){
+    if(n==1){
+        return 1;
+    }
+    else{
+        return n+sum(n-1);
+    }
+}
+
+//test
+var n=3;
+console.log(n+"!="+fact(n));
