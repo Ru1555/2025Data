6919f4095821c68aa556af4f46355127d5950e17
Ru1555 <ruuuu155313@gmail.com>
2025-10-07 15:36:17 +0800

    0336

--- fibonacci.js
+++ fibonacci.js
@@ -1,10 +1,17 @@
 function fib(n){
     //prevent n<0
-    if(n==0 || n==1){
-        return n;
-    }else{
-        return fib(n-1)+fib(n-2);
+    // if(n==0 || n==1){
+    //     return n;
+    // }else{
+    //     return fib(n-1)+fib(n-2);
+    // }
+
+    var a=0, b=1;
+    for(var i=1; i<=n; i++){
+        a = a+b;
+        b = a-b;
     }
+    return a;
 }
 
 //test
