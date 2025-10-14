d85319e4206a523b38fd1c513feaa674ffb70d8e
Ru1555 <ruuuu155313@gmail.com>
2025-10-14 16:06:01 +0800

    1014

--- 2Darray.js
+++ 2Darray.js
@@ -1,10 +1,18 @@
-var Score=[
-    [70,80,90,60,80],
-    [80,90,75,70,85],
-    [85,85,80,90,80]
-]
-console.log("Score[0]="+Score[0]);
+//var Score=[
+//    [70,80,90,60,80],
+//    [80,90,75,70,85],
+  //  [85,85,80,90,80]
+//]
+//console.log("Score[0]="+Score[0]);
 //avg for every element
+
+var Student=[
+    {"name":"jhon", "grade":2, "score":[70,80,90,60,80]},
+    {"name":"tom", "grade":2, "score":[80,90,70,75,85]},
+    {"name":"art", "grade":3, "score":[85,85,80,90,80]}
+]
+console.log("first student'sname="+Student[0].name)
+//show all student's name & avg
 
 for(let i=0; i<Score.length; i++){
     let sum=0;
@@ -12,5 +20,6 @@ for(let i=0; i<Score.length; i++){
         sum+=Score[i][j];
     }
     let avg=sum/Score[i].length;
-    console.log("第" + (i+1) + "列平均值為:" +avg.toFixed(2));
+    console.log("Student" + (i+1) + "avg:" +avg.toFixed(2));
 }
+
