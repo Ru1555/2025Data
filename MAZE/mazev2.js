aae951bb3a32c150a5cc2db2c6ab82b2f467ed1f
Ru1555 <ruuuu155313@gmail.com>
2025-12-02 16:06:30 +0800

    1202.0406

--- MAZE/mazev2.js
+++ MAZE/mazev2.js
@@ -1,17 +1,20 @@
-
 var MAZE = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1],
-    [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1],
     [1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1],
     [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1],
     [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1],
+    [1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1],
+    [1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
     [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1],
     [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ];//8,10
 
+class Point{
+    constructor(_row,_col){
+        this.row= _row;
+        this.col=_col;
+    }
+}
 class Point {
     constructor(_row, _col) {
         this.row = _row;
@@ -19,6 +22,38 @@ class Point {
     }
 }
 
+var start=new Point(1,1);
+var end = new Point(8,10);
+var Stack=[];
+var Direction=[[-1,0],[0,-1],[1,0],[0,1]].sort((a,b)=>{return Math.random()-0.5});
+
+var CP=start;//currentPosition
+do{
+    MAZE[CP.row][CP.col]=2;
+    //4 directions
+    if(MAZE[CP.row+Direction[0][0]][CP.col+Direction[0][1]]==0){//dir0
+        Stack.push(CP);
+        CP = new Point(CP.row+Direction[0][0],CP.col+Direction[0][1]);
+    }else if(MAZE[CP.row+Direction[1][0]][CP.col+Direction[1][1]]==0){ //dir1
+        Stack.push(CP);
+        CP = new Point(CP.row+Direction[1][0],CP.col+Direction[1][1]);
+    }else if(MAZE[CP.row+1][CP.col]==0){//dir2
+        Stack.push(CP);
+        CP = new Point(CP.row+1,CP.col);
+    }else if(MAZE[CP.row][CP.col+1]==0){//dir3
+        Stack.push(CP);
+        CP = new Point(CP.row,CP.col+1);
+    }else{
+        if(Stack.length>0){
+            //pop
+            CP=Stack.pop();
+        }
+        else{
+            //no solution
+            console.log("No solution!");
+            break;
+        }
+    }
 var start = new Point(1, 1);
 var end = new Point(8, 10);
 var Stack = [];
@@ -86,12 +121,29 @@ async function go() {
         }while(Stack.length>0);
 
     }
+//}while(!(CP.row==end.row && CP.col==end.col));
+}while(CP.row!=end.row || CP.col!=end.col);
 }
 
 function drawPoint(row, col, color) {
+    while (true) {
+        var canvas = document.getElementById("board").getContext("2d");
+        canvas.fillStyle = color;
+        canvas.fillRect(col * size, row * size, size, size);
+        canvas.strokeRect(col * size, row * size, size, size);
+        break; // Add a break to avoid an infinite loop
+    }
+}
     var canvas = document.getElementById("board").getContext("2d");
     var size = Math.floor(Math.min(canvas.canvas.height / MAZE.length, canvas.canvas.width / MAZE[0].length));
 
+
+if (Stack.length > 0) {
+    //print path 
+    Stack.push(end);
+    Stack.forEach((item, index) => {
+        console.log("Step " + (index + 1) + " to [" + item.row + "][" + item.col + "]");
+    });
     canvas.fillStyle = color;
 
     canvas.fillRect(col * size, row * size, size, size);
